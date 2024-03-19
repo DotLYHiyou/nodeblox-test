e4e68d48542f2941e79545feb70563f44cf395bc
DotLYHiyou <127796594+DotLYHiyou@users.noreply.github.com>
2024-03-19 11:41:46 +0000

    Pending changes exported from your codespace

---
+++ datastoreservicetest.js
@@ -0,0 +1,13 @@
+
+const noblox = require('noblox.js')
+async function startApp () {
+    // You MUST call setCookie() before using any authenticated methods [marked by 🔐]
+    // Replace the parameter in setCookie() with your .ROBLOSECURITY cookie.
+    const currentUser = await noblox.setCookie() 
+    console.log(`Logged in as ${currentUser.UserName} [${currentUser.UserID}]`)
+
+    // Do everything else, calling functions and the like.
+    const groupInfo = await noblox.getGroup()
+    console.log(groupInfo)
+}
+startApp()
